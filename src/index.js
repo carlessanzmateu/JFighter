01cf2b2b4b8ead3addb718c6b8c839d83aaa1df8
carles <carles.sanz.contractor@dev.bbva.com>
2018-12-04 16:08:24 +0100

    add background to the scene

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-// import './js/JFighter.js'
 require('./js/JFighter');
--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,15 @@
-function preload() {}
-function create() {}
+function preload() {
+  this.load.image('sky', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/background.png')
+  this.load.image('mountains', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/mountains.png')
+  this.load.image('graveyard', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/graveyard.png')
+}
+
+function create() {
+  this.add.image(400,300, 'sky').setScale(3)
+  this.add.image(0,200, 'mountains').setOrigin(0, 0).setScale(2.3)
+  this.add.image(400,200, 'mountains').setOrigin(0, 0).setScale(2.3)
+  this.add.image(0,350, 'graveyard').setOrigin(0, 0).setScale(2)
+}
 function update() {}
 
 const config = {
