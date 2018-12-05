9710304fe22987f6554106d734f0c856af29e4b1
carles <carles.sanz.contractor@dev.bbva.com>
2018-12-05 10:33:23 +0100

    integrate animations

--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 *webpack*
 https://www.valentinog.com/blog/webpack-tutorial/
+
+*animation without spritesheet*
+http://www.html5gamedevs.com/topic/36016-make-animation-from-separate-files-ie-not-spritesheet/
--- src/js/config.js
+++ src/js/config.js
@@ -1,12 +1,18 @@
 let platforms;
 let hero;
+let cursors;
 
 function preload() {
   this.load.image('sky', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/background.png')
   this.load.image('mountains', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/mountains.png')
   this.load.image('graveyard', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/graveyard.png')
   this.load.image('dirt', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/dirt.png')
-  this.load.image('heroIdle1', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Sprites/hero/hero-idle/hero-idle-1.png')
+  this.load.spritesheet('hero',
+    '/src/assets/Adventurer-1.5/adventurer-v1.5-Sheet.png',
+    {
+      frameWidth: 50,
+      frameHeight: 45
+  })
 }
 
 function create() {
@@ -26,12 +32,56 @@ function create() {
   platforms.create(552,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
   platforms.create(644,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
   platforms.create(736,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  
+  hero = this.physics.add.sprite(100, 450, 'hero')
 
-  hero = this.physics.add.image(100, 450, 'heroIdle1').setScale(2.3)
   hero.setBounce(0.2)
   hero.setCollideWorldBounds(true)
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    // frames: [ { key: 'hero', frame: 1 } ],
+    frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 3 }),
+    frameRate: 20
+});
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('hero', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  hero.body.setGravityY(300)
+
+  this.physics.add.collider(hero, platforms)
+
+  cursors = this.input.keyboard.createCursorKeys();
 }
-function update() {}
+
+function update() {
+  if (cursors.left.isDown) {
+    hero.setVelocityX(-160);
+    hero.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    hero.setVelocityX(160);
+    hero.anims.play('right', true);
+  } else {
+    hero.setVelocityX(0);
+    hero.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
 
 const config = {
   type: Phaser.AUTO,
