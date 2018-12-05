75aacf091c867660428f9449e44785fff1b4f629
carles <carles.sanz.contractor@dev.bbva.com>
2018-12-05 08:42:28 +0100

    add hero to the scene

--- src/js/config.js
+++ src/js/config.js
@@ -1,10 +1,12 @@
 let platforms;
+let hero;
 
 function preload() {
   this.load.image('sky', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/background.png')
   this.load.image('mountains', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/mountains.png')
   this.load.image('graveyard', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/graveyard.png')
   this.load.image('dirt', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Environment/dirt.png')
+  this.load.image('heroIdle1', '/src/assets/gothicvania-cemetery-files/gothicvania-cemetery-files/PNG/Sprites/hero/hero-idle/hero-idle-1.png')
 }
 
 function create() {
@@ -12,19 +14,22 @@ function create() {
   this.add.image(0,200, 'mountains').setOrigin(0, 0).setScale(2.3)
   this.add.image(400,200, 'mountains').setOrigin(0, 0).setScale(2.3)
   this.add.image(0,350, 'graveyard').setOrigin(0, 0).setScale(2)
-  // this.add.image(0,525, 'dirt').setOrigin(0, 0).setScale(1.5)
 
   platforms = this.physics.add.staticGroup();
 
-  platforms.create(0,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(92,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(184,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(276,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(368,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(460,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(552,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(644,525, 'dirt').setOrigin(0, 0).setScale(1.5)
-  platforms.create(736,525, 'dirt').setOrigin(0, 0).setScale(1.5)
+  platforms.create(0,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(92,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(184,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(276,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(368,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(460,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(552,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(644,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+  platforms.create(736,525, 'dirt').setOrigin(0, 0).setScale(1.5).refreshBody()
+
+  hero = this.physics.add.image(100, 450, 'heroIdle1').setScale(2.3)
+  hero.setBounce(0.2)
+  hero.setCollideWorldBounds(true)
 }
 function update() {}
 
